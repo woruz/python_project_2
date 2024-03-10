fcce6f28d4d452109f9dcf7f0c62760d252b5d9e
sabil <sabil.danish1997@gmail.com>
2024-03-10 12:04:04 +0530

    python intermediate project

---
+++ project2.py
@@ -0,0 +1,29 @@
+with open('story.txt','r') as f:
+    story = f.read()
+
+words = set()
+start_of_word = -1
+
+target_start = "<"
+target_end = ">"
+
+for i,char in enumerate(story):
+    if char == target_start:
+        start_of_word = i
+
+    if char == target_end and start_of_word != -1:
+        word = story[start_of_word: i+1]
+        words.append(word)
+        start_of_word = -1
+
+
+answer = {}
+
+for word in words:
+    answer = input("Enter a word for " + word + ": ")
+    answer[word] = answer
+
+for word in words:
+    story = story.replace(word, answer[word])
+
+print(story)
